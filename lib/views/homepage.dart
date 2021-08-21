1e68694be3f7796f0355c645ea33622f501394eb
bashiryousufy <bashiryousufy@gmail.com>
2021-08-21 11:05:56 +0800

    added homepage ui

---
+++ lib/views/homepage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: Icon(
+          Icons.arrow_back_ios,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'ShopX',
+                    style: TextStyle(
+                        fontFamily: 'avenir',
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(Icons.view_list_rounded), onPressed: () {}),
+                IconButton(icon: Icon(Icons.grid_view), onPressed: () {}),
+              ],
+            ),
+          ),
+          Expanded(
+            child: StaggeredGridView.countBuilder(
+              crossAxisCount: 2,
+              itemCount: 100,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                );
+              },
+              staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
